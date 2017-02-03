b8c71e670bd6d067b7eb41125aceb1e0046ed1e9
Ken V <kvaczi2@gmail.com>
2017-02-03 11:12:58 -0600

    remove confirmable email for the time being until we determine user strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :omniauthable #, :confirmable # TODO: Determine what to do with confirmable
   include DeviseTokenAuth::Concerns::User
 
   has_many :recipes, dependent: :destroy
